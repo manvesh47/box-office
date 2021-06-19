39eb2b40bd1264d3b8826e591f97ee16ba2899a3
caedes <manveshgill47@gmail.com>
2021-06-19 11:13:01 +0530

    created search api

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,9 +1,41 @@
-/* eslint-disable arrow-body-style */
-import React from 'react';
+import React, { useState } from 'react';
 import MainPageLayout from '../components/MainPageLayout';
 
 const Home = () => {
-  return <MainPageLayout>this is our home pages</MainPageLayout>;
+  // eslint-disable-next-line no-unused-vars
+  const [input, setInput] = useState('');
+
+  const onInputChange = ev => {
+    setInput(ev.target.value);
+  };
+
+  const Onsearch = () => {
+    fetch(`https://api.tvmaze.com/search/shows?q=${input}`)
+      .then(r => r.json())
+      .then(result => {
+        console.log(result);
+      });
+  };
+
+  const onKeyDown = ev => {
+    if (ev.keyCode === 13) {
+      Onsearch();
+    }
+  };
+
+  return (
+    <MainPageLayout>
+      <input
+        type="text"
+        onChange={onInputChange}
+        onKeyDown={onKeyDown}
+        value={input}
+      />
+      <button type="button" onClick={Onsearch}>
+        Search
+      </button>
+    </MainPageLayout>
+  );
 };
 
 export default Home;
